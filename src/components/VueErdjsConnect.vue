d07f30ee79395ef7bdd842c2c8819a42def4cecd
p0ulpe <delanoe.william@gmail.com>
2022-01-08 23:56:10 +0100

    Responsive desktop

--- src/components/VueErdjsConnect.vue
+++ src/components/VueErdjsConnect.vue
@@ -1,38 +1,61 @@
 <template>
     <div class="vue3rdj5">
-      <div class="vue3rdj5__logged" v-if="$erd.walletAddress">
-        <div class="vue3rdj5__logged-address">{{$erd.obfuscatedWalletAddress}}</div>
-        <button class="vue3rdj5__logged-logout" @click.prevent="$erd.logout()">Logout</button>
-      </div>
-      <div v-if="!$erd.walletAddress">
-        <defi-wallet-login></defi-wallet-login>
-        <maiar-login></maiar-login>
-        <ledger-login></ledger-login>
-        <web-wallet-login></web-wallet-login>
-      </div>
+        <div class="vue3rdj5__logged" v-if="$erd.walletAddress">
+            <div class="vue3rdj5__logged-address">{{$erd.obfuscatedWalletAddress}}</div>
+            <button class="vue3rdj5__logged-logout" @click.prevent="$erd.logout()">Logout</button>
+        </div>
+        <div
+            class="vue3rdj5__modes"
+            v-if="!$erd.walletAddress">
+            <defi-wallet-login
+                @select-mode="selectedMode = $event"
+                :selected-mode="selectedMode"></defi-wallet-login>
+            <vue-erdjs-tab
+                name="Maiar"
+                @select-mode="selectedMode = $event"
+                :selected-mode="selectedMode"></vue-erdjs-tab>
+            <maiar-login
+                :selected-mode="selectedMode"></maiar-login>
+            <vue-erdjs-tab
+                name="Ledger"
+                @select-mode="selectedMode = $event"
+                :selected-mode="selectedMode"></vue-erdjs-tab>
+            <ledger-login
+                :selected-mode="selectedMode"></ledger-login>
+            <web-wallet-login
+                @select-mode="selectedMode = $event"
+                :selected-mode="selectedMode"></web-wallet-login>
+        </div>
     </div>
 </template>
 
 <script>
+import VueErdjsTab from './VueErdjsTab.vue';
 export default {
-  name:"VueErdjsConnect",
-  mounted() {
-    this.redirect();
-  },
-  methods: {
-    redirect() {
-      if (this.$router && this.$erd.walletAddress && this.$route.query.fromUrl) {
-        this.$router.push(this.$route.query.fromUrl);
-      }
-    }
-  },
-  watch: {
-    "$erd.walletAddress": (address) => {
-      if(address != null && this){
+    components: { VueErdjsTab },
+    name:"VueErdjsConnect",
+    data () {
+        return {
+            selectedMode: ''
+        }
+    },
+    mounted() {
         this.redirect();
-      }
+    },
+    methods: {
+        redirect() {
+            if (this.$router && this.$erd.walletAddress && this.$route.query.fromUrl) {
+                this.$router.push(this.$route.query.fromUrl);
+            }
+        }
+    },
+    watch: {
+        "$erd.walletAddress": (address) => {
+            if(address != null && this){
+                this.redirect();
+            }
+        }
     }
-  }
 }
 </script>
 
