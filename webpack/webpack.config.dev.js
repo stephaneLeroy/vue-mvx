bc21668896e166518f7a117658d5e076dd7a7534
Stéphane Leroy <ste.leroy@gmail.com>
2022-01-10 21:50:12 +0100

    Prepare github page

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -3,8 +3,9 @@ var webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {merge} = require("webpack-merge")
 const common = require("./webpack.config.common")
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(common,
+const webpack_config = merge(common,
     {
         mode: 'development',
         entry: './example/main.ts',
@@ -36,3 +37,23 @@ module.exports = merge(common,
         devtool: 'inline-source-map'
     }
 )
+
+if (process.env.BUILD_TARGET === 'github') {
+    webpack_config.optimization = {
+        minimize: true,
+            usedExports: false,
+            minimizer: [
+            new TerserPlugin({
+                include: /\.js$/,
+                extractComments: false,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                }
+            }),
+        ]
+    }
+}
+
+module.exports = webpack_config;
