6c99aa68b81f9d0b7affafebb20be4ae2d2e284f
Stéphane Leroy <ste.leroy@gmail.com>
2022-09-27 02:39:00 +0200

    Add types and some cleaning

--- example/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-import VueErdJsPlugin, {ElrondEnvEnum, providersOptions} from '../src'
-
-const app = createApp(App);
-app.use(VueErdJsPlugin, providersOptions(ElrondEnvEnum.DEVNET));
-app.use(router);
-app.mount('#app');
--- example/router/CustomQRCodeHandler.d.ts
+++
@@ -1,6 +0,0 @@
-import { IQRCodeHandler } from "../../src";
-declare class CustomQRCodeHandler implements IQRCodeHandler {
-    private qrcode;
-    handle(data: string, element: HTMLElement): Promise<any>;
-}
-export default CustomQRCodeHandler;
