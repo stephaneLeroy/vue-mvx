85fb1cb427dd48981bb38e9d3750926bb8244ac0
Well <niftyrexnft@gmail.com>
2023-11-27 19:18:59 +0100

    2FA modal scss styling

--- vue-mvx/src/components/VueErdjs2FA.vue
+++ vue-mvx/src/components/VueErdjs2FA.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="modal" v-if="showModal" @click="closeModal">
-        <div class="modal-content" @click.stop>
-            <button class="close-btn" @click="closeModal">X</button>
+    <div class="vue3rdj5__modal" v-if="showModal" @click="closeModal">
+        <div class="vue3rdj5__modal__content" @click.stop>
+            <button class="vue3rdj5__modal__content__close-button" @click="closeModal">X</button>
             <h2>Two-Factor Authentication</h2>
             <section v-if="isCodeValid">
                 <h3>Valid 2FA code</h3>
@@ -9,7 +9,7 @@
                 <div class="loader"></div>
             </section>
             <section v-else>
-                <div class="code-input">
+                <div class="vue3rdj5__modal__content__code-input">
                     <input
                         v-for="index in 3"
                         :key="'code' + index"
@@ -20,7 +20,7 @@
                         type="text"
                         maxlength="1"
                     />
-                    <span class="auth-span">-</span>
+                    <span class="vue3rdj5__modal__content__auth-span">-</span>
                     <input
                         v-for="index in [4, 5, 6]"
                         :key="'code' + index"
@@ -32,12 +32,12 @@
                         maxlength="1"
                     />
                 </div>
-                <div v-if="hasError" class="error-message">
+                <div v-if="hasError" class="vue3rdj5__modal__content__error-message">
                     Wrong 2FA code? Please try again.
                 </div>
-                <a @click="resetCode">Reset</a>
+                <a class="vue3rdj5__modal__content__reset" @click="resetCode">Reset</a>
                 <button
-                    class="submit-button"
+                    class="vue3rdj5__modal__content__submit-button"
                     @click="submitCode"
                     :disabled="isSubmitDisabled"
                 >
@@ -144,104 +144,3 @@ const closeModal = () => {
     EventBus.emit("closed")
 }
 </script>
-
-<style scoped>
-.modal {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 1000; /* Set a high z-index value */
-}
-
-.modal-content {
-    background-color: white;
-    padding: 20px;
-    border-radius: 5px;
-    text-align: center;
-    position: relative;
-}
-
-input {
-    margin-bottom: 10px;
-    height: 20px;
-    margin-right: 2px;
-    margin-left: 2px;
-    padding-top: 10px;
-    width: 20px !important;
-    padding-bottom: 10px;
-    font-size: 16px;
-}
-
-button {
-    padding: 10px;
-    font-size: 16px;
-    margin-right: 10px;
-}
-
-a {
-    font-size: 16px;
-    color: blue;
-    cursor: pointer;
-}
-
-.close-btn {
-    position: absolute;
-    top: 0;
-    right: 0;
-    border: none;
-    background-color: transparent;
-    font-size: 20px;
-    cursor: pointer;
-}
-
-.code-input {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-input {
-    width: 40px;
-    text-align: center;
-}
-
-.auth-span {
-    margin: 10px 5px 20px 5px;
-}
-
-.submit-button {
-    display: inline-block;
-    border: none; /* Remove border */
-    border-radius: 0.375rem; /* 6px */
-    padding: 0.5rem 0.75rem; /* 8px 12px */
-    text-align: center;
-    text-decoration: none;
-    margin-bottom: 0;
-    margin-left: 20px;
-    background-color: #007bff; /* Bootstrap blue */
-    color: #ffffff;
-    cursor: pointer;
-}
-
-.submit-button:hover {
-    background-color: #0056b3; /* Darker blue */
-}
-
-.submit-button:disabled {
-    background-color: #e2e2e2; /* Light gray */
-    color: #000000;
-    cursor: not-allowed;
-}
-
-.error-message {
-    color: rgb(255, 100, 100);
-    font-size: small;
-    margin-bottom: 10px;
-}
-</style>
